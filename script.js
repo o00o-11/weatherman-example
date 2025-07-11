7f9e29eff7f465e7560db1f291939fc4e54f0997
o00o-11 <gluck_94@naver.com>
2025-07-11 15:40:24 +0900

    250711

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 const getCurrenWeather = (lat, lon) => {
-  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4a3724571f65ec296cde31a258ed847b`;
+  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4a3724571f65ec296cde31a258ed847b&units=metric`;
 
-  console.log(URL);
-}
+  fetch(URL)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+
+      const temp = document.querySelector(".temp");
+      temp.innerText = `${result.main.temp}℃`;
+
+      let weatherInfo;
+      const weather = document.querySelector(".weather");
+      switch(result.weather[0].main) {
+        case "Clear":
+          weatherInfo = "🌈 맑음";
+      }
+      weather.innerText = weatherInfo;
+
+      let cityName;
+      const city = document.querySelector(".city");
+      switch(result.name) {
+        case "Jamwon-dong" :
+          cityName = "🏢 역삼동"
+      }
+      city.innerText = cityName;
+
+      const icon = document.querySelector(".icon");
+      icon.src = `https://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png`
+  });
+};
 
 const getPosition = (position) => {
   const { latitude, longitude } = position.coords;

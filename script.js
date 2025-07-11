ec7b31a0cab972c857614d9b3877e82750684a45
o00o-11 <gluck_94@naver.com>
2025-07-11 13:51:52 +0900

    fistedit

---
+++ script.js
@@ -0,0 +1,15 @@
+const getPosition = (position) => {
+  const { latitude, longitude } = position.coords;
+  console.log("위도 : ", latitude);
+  console.log("경도 : ", longitude);
+};
+
+const errorHandle = (error) => {
+  console.error(error);
+};
+
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(getPosition, errorHandle)
+} else {
+  console.log("geolocation is not available");
+}
